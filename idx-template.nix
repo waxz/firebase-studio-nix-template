8f961bca2c2b7e195e3063ff0e6ebb043a7c1191
wonder <waxz@users.noreply.github.com>
2025-10-21 06:38:33 +0000

    add fastapi

--- idx-template.nix
+++ idx-template.nix
@@ -10,6 +10,7 @@
     shopt -s dotglob; cp -rf ${./.}/${environment}/dev/* "$out"
     chmod -R u+w "$out"
     cp -rf ${./.}/update-nix.sh "$out"
+    chmod +x "$out"/*.sh
     cd "$out" && bash ./update-nix.sh && rm ./update-nix.sh
   '';
 }
