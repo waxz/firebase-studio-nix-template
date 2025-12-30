0a4989f8a4a368273da00ff6fbdb723cbce13267
wonder <waxz@users.noreply.github.com>
2025-12-30 00:07:08 -0500

    Clean up whitespace in dev.nix

--- base/dev.nix
+++ base/dev.nix
@@ -1,23 +1,21 @@
 { pkgs, ... }: {
   channel = "stable-23.11";
-  
+
   packages = [
     # Packages will be inserted here by bootstrap
   ];
 
   idx = {
     extensions = [
-    # Extensions will be inserted here by bootstrap
-      
+      # Extensions will be inserted here by bootstrap
     ];
-    
+
     previews = {
       enable = true;
     };
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
-
       };
     };
   };
